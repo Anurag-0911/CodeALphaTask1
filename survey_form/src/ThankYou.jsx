ab64c69741008f94802c1c4b244655116fbf7914
Anurag Kumar <anuraganu202016@gmail.com>
2023-08-14 12:18:36 +0530

    last page

--- survey_form/src/ThankYou.jsx
+++ survey_form/src/ThankYou.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import img1 from './image/background6.jpg'
+
+
 
 export default function ThankYou() {
-  return (
-    <div>
-       Submission Completed 
-    </div>
-  )
+  return (<>
+    <img src={img1} alt = ""/>
+      <div className='tyb'>
+        <h1 className='ty'> Submission Completed<span id='tnq'><br></br>Thank You </span></h1>
+      </div>
+      </>
+    )
 }
